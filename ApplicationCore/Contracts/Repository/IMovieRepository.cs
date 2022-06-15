5b04b9990685ba58651698d640b2fe37d29e9ac5
Sean Suh <gliese581ge@gmail.com>
2022-06-14 21:04:59 -0700

    first commit

---
+++ ApplicationCore/Contracts/Repository/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Repository
+{
+    public interface IMovieRepository
+    {
+        // should have all the database logic methods for Movies
+
+        // Movie GetMovie(int id)
+        // MovieModel GetMoiveDetails(int movieId)
+    }
+}
